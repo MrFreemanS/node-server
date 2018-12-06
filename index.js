75d43bda8ddce5ab60ed1d6d5144c2941e163dbd
Svyatoslav <moderator04@mail.ru>
2018-12-06 11:01:51 +0300

    add json

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
-
+var bodyParser = require('body-parser')
 var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
 	res.send('Hello API');
