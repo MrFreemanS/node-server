15bbb50fdf9cd79c8da282cccfa75f00a7fd387e
Svyatoslav <moderator04@mail.ru>
2018-12-24 02:38:23 +0300

    edit mysql

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-
+const mysql = require('mysql');
+const express = require('express');
 var app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
 
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "myusername",
-  password: "mypassword"
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'api_db',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB connection succeded.');
+    else
+        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
 });
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
-	res.send('Hello API');
+//Get all news
+app.get('/news', (req, res) => {
+    mysqlConnection.query('SELECT * FROM news LIMIT 10', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
 });
 
-app.get('/news/:id', function(req, res) {
-	res.send('news '+req.params.id);
+
+//Get an news
+app.get('/news/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM news WHERE news_id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
 });
-app.listen(3012, function() {
-	console.log('API app started');
+
+
+//Delete an news
+app.delete('/news/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM news WHERE news_id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted successfully.');
+        else
+            console.log(err);
+    })
 });
+
+app.listen(3012, () => console.log('Express server is runnig at port no : 3012'));
